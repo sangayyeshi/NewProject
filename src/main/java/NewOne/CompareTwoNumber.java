ec94dd85d3652fc4042d0b403d9cc41d1f117271
sangay yeshi <sangayyeshi@sangays-MacBook-Pro.local>
2026-01-22 02:57:15 +0300

    add the two number and getting the sum of that number

---
+++ src/main/java/NewOne/CompareTwoNumber.java
@@ -0,0 +1,11 @@
+package NewOne;
+
+public class CompareTwoNumber {
+    static void main(){
+         // get  the sum of two number 74and 36
+        int a= 74;
+         int b = 36;
+          int c = a+b;
+           System.out.println(c);
+    }
+}
