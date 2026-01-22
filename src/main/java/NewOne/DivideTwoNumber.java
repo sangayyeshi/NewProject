892fa6b1cea3f378f00993d1cd6c5558e86fbff1
sangay yeshi <sangayyeshi@sangays-MacBook-Pro.local>
2026-01-22 03:02:21 +0300

    dividing  the number

---
+++ src/main/java/NewOne/DivideTwoNumber.java
@@ -0,0 +1,11 @@
+package NewOne;
+
+public class DivideTwoNumber {
+   static void main(){
+       // divide the two number and get the result 50/3
+       int x = 50;
+       int y = 3;
+       int z = x/y;
+       System.out.println(z);
+    }
+}
