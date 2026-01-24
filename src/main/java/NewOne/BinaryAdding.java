dc1adac2894cabcd6441a00783d3ba406c4d1e39
sangay yeshi <sangayyeshi@sangays-MacBook-Pro.local>
2026-01-24 03:14:26 +0300

    resversed string in java

---
+++ src/main/java/NewOne/BinaryAdding.java
@@ -0,0 +1,8 @@
+package NewOne;
+
+public class BinaryAdding {
+    static void main(){
+        System.out.println("\n java version: " + System.getProperty("java.version"));
+        System.out.println("\nJava Version: " + System.getProperty("java.version"));
+    }
+}
